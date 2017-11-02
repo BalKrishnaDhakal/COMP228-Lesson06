8349394d6e99c5d400ed3c5ac2aa699c66bcdf33
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-11-02 14:03:51 -0400

    First Commit

---
+++ src/program.java
@@ -0,0 +1,22 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class program {
+
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame();
+		JPanel myPanel = new JPanel();
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setBounds(300, 300, 600, 600);
+		
+		myPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		myPanel.setLayout(new BorderLayout(0, 0));
+		myFrame.setContentPane(myPanel);
+		myFrame.setVisible(true);
+		
+	}
+
+}
